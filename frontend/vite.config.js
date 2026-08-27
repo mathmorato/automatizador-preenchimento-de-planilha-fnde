1404f16b8d0df5b7be4d4d6b0c1330d8c05a483d
mathmorato <morato@ufg.br>
2026-08-27 18:25:14 -0300

    Fix: ajustando execução local.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: './',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -13,4 +13,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
